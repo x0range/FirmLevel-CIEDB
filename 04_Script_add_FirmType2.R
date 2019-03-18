78d537cf5c14dd5b3a2177f28aa78b2f3477f592
Torsten <torsten.heinrich@posteo.net>
2019-03-18 12:08:01 +0100

    Added distinction of state owned and collective owned, added 'other domestic' category

--- 04_Script_add_FirmType2.R
+++ 04_Script_add_FirmType2.R
@@ -11,11 +11,13 @@ setwd("/home/mercury/dat/CIEDB_2009_2013/")
 load("China_data_set_incl_compounds.Rda", verbose=T)
 df$FirmType2 <- NA
 
-df <- function_set_FirmType2(df, c(110,120,130,151), "SOE")
-df <- function_set_FirmType2(df, c(159,160), "Other Limited")
+df <- function_set_FirmType2(df, c(110,141,151), "SOE")
+df <- function_set_FirmType2(df, c(120,130,142), "Collective owned")
+df <- function_set_FirmType2(df, c(159,160), "Shareholding")
 df <- function_set_FirmType2(df, c(171,172,173,174), "Private")
 df <- function_set_FirmType2(df, c(210,220,230,240,290), "HK, Macao, Taiwan owned")
 df <- function_set_FirmType2(df, c(310,320,330,340,390), "Foreign owned")
+df <- function_set_FirmType2(df, c(143, 149, 190), "Other domestic")
 
 print(table(df$FirmType2))
 
